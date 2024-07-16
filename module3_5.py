3dad2fdbe9998eaf357c4c3fccf0a2b87ffde6c9
dmitri2309 <kerzhimanov@gmail.com>
2024-07-16 16:19:33 +0300

    Add files via upload

---
+++ module3_5.py
@@ -0,0 +1,14 @@
+def get_multiplied_digits(number):
+    str_number = str(number)
+    first = int(str_number[0])
+    if len(str_number) <= 1:
+        return first
+    else:
+        n = first * get_multiplied_digits(int(str_number[1:]))
+        return n
+result = get_multiplied_digits(40203)
+print(result)
+
+
+
+
